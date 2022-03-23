efc6e0b86c3f122ae35ac15cee3ffaac3174d9f6
unknown <chilamakooru.gangaiiit@gmail.com>
2022-03-23 16:45:14 +0530

    Creating Employee_Payroll table

--- employee_Payroll.sql
+++ employee_Payroll.sql
@@ -1,2 +1,13 @@
 ﻿--UC1 Creating payroll_database 
 create database payroll_service
+
+--UC2 Creating employee_payroll table
+use payroll_service
+
+create table employee_payroll
+(
+  id int primary key identity,
+  name varchar (20) not null,
+  salary decimal(10,2) not null,
+  startDate date not null
+);
